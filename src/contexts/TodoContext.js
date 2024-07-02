ad7dc519b6ba8f083e03918908af34b2f3556ae9
Pranav <pranavarjun14@gmail.com>
2024-07-02 17:38:00 +0530

    todo using react js

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext , useContext  } from "react";
+
+export const TodoContext = createContext({
+    todos :[
+        {
+            id : 1 ,
+            todo : "Msg",
+            completed : false
+        }
+
+    ],
+
+    addTodo : (todo)=>{},
+    deleteTodo : (id)=>{},
+    updateTodo : (id,todo)=>{},
+    toggleComplete :(id)=>{}
+})
+
+export const TodoProvider = TodoContext.Provider
+
+export default function useTodo(){
+    return useContext(TodoContext)
+}
